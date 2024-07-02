064fd6431aa3588cebabd6fbe365b0527862069b
siddhu2015 <rawalsiddhu2015@gmail.com>
2024-07-02 21:48:18 +0545

    Migration done

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env("DJANGO_DEBUG")
 
 ALLOWED_HOSTS=["localhost","127.0.0.1:8000", "taskmatee.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ['https://taskmatee.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://taskmatee.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
